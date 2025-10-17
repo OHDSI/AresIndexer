9139805d4261e9bbf0f0ef5c3024bed2fcf15a80
Mikhail-iontsev <mikhail_iontsev@icloud.com>
2025-10-17 22:35:05 +0800

    improvement: isError, passed and notApplicable used in addition to failed to calculate delta

--- R/AugmentDataQualityFiles.R
+++ R/AugmentDataQualityFiles.R
@@ -1,6 +1,5 @@
 # @file augmentDataQualityFiles.R
 #
-#
 # Copyright 2021 Observational Health Data Sciences and Informatics
 #
 # This file is part of AresIndexer
@@ -34,56 +33,111 @@
 augmentDataQualityFiles <- function(sourceFolders) {
   loadDataQualityFile <- function(dir) {
     filePath <- file.path(dir, "dq-result.json")
-    fromJSON(filePath)
+    fromJSON(filePath, simplifyVector = TRUE)
   }
 
-  for(sourceFolder in sourceFolders) {
+  isFailure <- function(failed, isError = NA, passed = NA, notApplicable = NA) {
+    dplyr::case_when(
+      !is.na(isError) & isError == 1 ~ TRUE,
+      !is.na(passed) & passed == 1 ~ FALSE,
+      !is.na(notApplicable) & notApplicable == 1 ~ FALSE,
+      failed == 1 ~ TRUE,
+      TRUE ~ FALSE
+    )
+  }
+
+  for (sourceFolder in sourceFolders) {
     writeLines(paste0('Augmenting data quality files for: ', basename(sourceFolder)))
     releases <- list.dirs(sourceFolder, recursive = FALSE)
     loadedData <- list()
 
     for (i in seq_along(releases)) {
       currentReleaseName <- basename(releases[[i]])
-      writeLines(paste0('Processing issues delta for release: ',currentReleaseName))
+      writeLines(paste0('Processing issues delta for release: ', currentReleaseName))
       currentQualityFile <- loadDataQualityFile(releases[i])
       currentChecks <- currentQualityFile$CheckResults
       currentChecks$checkId <- as.character(currentChecks$checkId)
+
+      if ("delta" %in% names(currentChecks)) {
+        currentChecks$delta <- NULL
+      }
+
+      originalColumns <- names(currentChecks)
 
       loadedData[[length(loadedData) + 1]] <- currentChecks
 
       if (length(loadedData) > 1) {
         previousData <- loadedData[[length(loadedData) - 1]]
+        previousData$checkId <- as.character(previousData$checkId)
 
-        commonCols <- intersect(names(currentChecks), names(previousData))
-        currentChecks <- currentChecks[, commonCols, drop = FALSE]
-        previousData <- previousData[, commonCols, drop = FALSE]
+        if ("delta" %in% names(previousData)) {
+          previousData$delta <- NULL
+        }
+
+        missingColsCurr <- setdiff(names(previousData), names(currentChecks))
+        missingColsPrev <- setdiff(names(currentChecks), names(previousData))
+
+        for (col in missingColsCurr) {
+          currentChecks[[col]] <- NA
+        }
+        for (col in missingColsPrev) {
+          previousData[[col]] <- NA
+        }
+
+        currentChecks <- currentChecks[, sort(names(currentChecks))]
+        previousData <- previousData[, sort(names(previousData))]
 
         mergedData <- currentChecks %>%
-          left_join(previousData, by = "checkId", suffix = c("", "_previous"))
+          dplyr::left_join(previousData, by = "checkId", suffix = c("", "_previous")) %>%
+          dplyr::mutate(
+            failed_now = isFailure(
+              .data$failed,
+              if("isError" %in% names(.)) .data$isError else NA,
+              if("passed" %in% names(.)) .data$passed else NA,
+              if("notApplicable" %in% names(.)) .data$notApplicable else NA
+            ),
 
-        mergedData <- mergedData %>%
-          mutate(delta = case_when(
-            is.na(.data$failed_previous) & .data$failed == 1 ~ "NEW",
-            .data$failed == 1 & .data$failed_previous == 0 ~ "NEW",
-            .data$failed == 1 & .data$failed_previous == 1 ~ "EXISTING",
-            .data$failed == 0 & .data$failed_previous == 1 ~ "RESOLVED",
-            .data$failed == 0 & .data$failed_previous == 0 ~ "STABLE",
-            TRUE ~ "STABLE" # Default case if none of the above match
-          )) %>%
-          select(-ends_with("_previous"))
+            failed_prev = isFailure(
+              .data$failed_previous,
+              if("isError_previous" %in% names(.)) .data$isError_previous else NA,
+              if("passed_previous" %in% names(.)) .data$passed_previous else NA,
+              if("notApplicable_previous" %in% names(.)) .data$notApplicable_previous else NA
+            ),
+
+            delta = dplyr::case_when(
+              is.na(.data$failed_previous) & failed_now ~ "NEW",
+              failed_now  & !failed_prev ~ "NEW",
+              failed_now  &  failed_prev ~ "EXISTING",
+              !failed_now &  failed_prev ~ "RESOLVED",
+              !failed_now & !failed_prev ~ "STABLE",
+              TRUE ~ "STABLE"
+            )
+          )
+
+        columnsToKeep <- c(originalColumns, "delta")
+        mergedData <- mergedData[, columnsToKeep[columnsToKeep %in% names(mergedData)]]
 
         currentQualityFile$CheckResults <- mergedData
+
       } else {
         currentChecks <- currentChecks %>%
-          mutate(delta = ifelse(.data$failed == 1, "NEW", "STABLE"))
+          dplyr::mutate(
+            is_failed = isFailure(
+              .data$failed,
+              if("isError" %in% names(.)) .data$isError else NA,
+              if("passed" %in% names(.)) .data$passed else NA,
+              if("notApplicable" %in% names(.)) .data$notApplicable else NA
+            ),
+            delta = ifelse(is_failed, "NEW", "STABLE")
+          ) %>%
+          dplyr::select(-is_failed)
 
         currentQualityFile$CheckResults <- currentChecks
       }
 
-      jsonlite::toJSON(currentQualityFile) |>
-      write(file.path(releases[i], "dq-result.json"))
+      json <- jsonlite::toJSON(currentQualityFile, pretty = TRUE, auto_unbox = TRUE, null = "null")
+      write(json, file.path(releases[i], "dq-result.json"))
 
-      # Maintain only the last two loaded datasets
       if (length(loadedData) > 2) {
         loadedData <- loadedData[-1]
       }
